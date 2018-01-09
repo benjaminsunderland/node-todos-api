db79f06ca9dd5097812c1b43b4d0c8488df1e116
benjaminsunderland <ben@sunderland.eu.com>
2018-01-09 20:17:59 +0000

    Added two collections

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -5,5 +5,32 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     return console.log(err);
   }
     console.log("Success: Connected, baby.");
+
+    db.collection('Todos').insertOne({
+
+      text: 'Something to do',
+      completed: false
+
+    }, (err, result) => {
+      if (err) {
+        console.log(err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+
+    db.collection('Users').insertOne({
+
+      name: "Benjamin Sunderland",
+      age: 25,
+      location: "I don't even know where I am"
+
+    }, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(JSON.stringify(result.ops, undefined, 2));
+    });
     db.close();
+
 });
