ae90e0e36c41267834e2185cab236bd8c9892f61
benjaminsunderland <ben@sunderland.eu.com>
2018-01-10 11:40:24 +0000

    Added database queries

---
+++ playground/mongodb-find.js
@@ -0,0 +1,21 @@
+// const MongoClient = require("mongodb").MongoClient;
+const {MongoClient, ObjectID} = require("mongodb");
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log(err);
+  }
+    console.log("Success: Connected, baby.");
+    //
+    // db.collection("Todos").find().count().then((count) => {
+    //   console.log(count);
+    //   console.log(JSON.stringify(docs, undefined, 2))
+    // }, (err) => {
+    //   console.log("Error", err);
+    // });
+
+    db.collection("Users").find({name: "Benjamin Sunderland"}).toArray().then((docs) => {
+      console.log(JSON.stringify(docs, undefined, 2));
+    });
+    // db.close();
+});
