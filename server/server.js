2a2c249cd5e4fd069c1962b7db621b7d7bfb3d50
benjaminsunderland <ben@sunderland.eu.com>
2018-01-12 12:22:01 +0000

    Changed port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
 
 var app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -48,7 +48,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Started on port ${port}`)
 });
 
